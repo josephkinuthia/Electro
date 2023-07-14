1d76fb9e92259e8b23f121200f51dcb7635a5454
watiri123 <joywatiri81gmail.com>
2023-07-14 08:04:18 +0300

    electro

---
+++ app/src/main/java/com/example/masoko/schoolsplash.kt
@@ -0,0 +1,11 @@
+package com.example.masoko
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class schoolsplash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schoolsplash)
+    }
+}
